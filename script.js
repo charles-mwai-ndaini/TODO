b976544b67ffb1a06dd851a95a7c9ad60a30605c
charles-mwai-ndaini <charlesmwai9025@gmail.com>
2023-03-24 10:43:28 +0300

    todo

---
+++ script.js
@@ -0,0 +1,42 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskButton = document.getElementById("TaskButton");
+const taskList = document.getElementById("taskList");
+
+addTaskButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    if (taskInput.value === "") {
+        alert("You must write something!");
+    } else {
+        const newTask = document.createElement("li");
+        const editInnerText = taskInput.value;
+        newTask.innerText = taskInput.value;
+        taskList.appendChild(newTask);
+				taskInput.value = "";
+
+        const cancelButton = document.createElement("button");
+        cancelButton.innerText = "X";
+        cancelButton.className = "cancel-button";
+        newTask.appendChild(cancelButton);
+        cancelButton.addEventListener("click", function() {
+            taskList.removeChild(newTask);
+        });
+
+        const tickButton = document.createElement("button");
+        tickButton.innerText = "✔";
+        tickButton.className = "tick-button";
+        newTask.appendChild(tickButton);
+        tickButton.addEventListener("click", function() {
+            newTask.classList.toggle("completed");
+        });
+
+        const editButton = document.createElement("button");
+        editButton.innerText = "Edit";
+        editButton.className = "edit-button";
+        newTask.appendChild(editButton);
+        editButton.addEventListener("click", function() {
+            taskInput.value = editInnerText;
+            taskInput.focus();
+            taskList.removeChild(newTask);
+        });
+    }
+});
